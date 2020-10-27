b7703b7b721f112f44c3f585841da0bb5660fe34
ChristyTropila <tropila04@gmail.com>
2020-10-27 13:23:26 -0400

    associations working correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reservations
-    has_many :listings, through: :reservations
+    has_many :listings
     has_many :reviews
-    has_many :listings, through: :reviews
+   # has_many :listings, through: :reviews
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.destroy_all
+
+
+Listing.destroy_all
+
+
+User.reset_pk_sequence
+Listing.reset_pk_sequence
+
+test=User.create!(name: "christy", email: "test@test.com", password: "123", age: "29", contact: "371737373")
+test2=User.create!(name: "Kim", email: "test2@test.com", password: "123", age: "19", contact: "467747473")
+
+listing=Listing.create!(user_id: test.id, lat: "393.333", lng: "-73.324", available: true)
+reserv=Reservation.create!(user_id: test2.id, listing_id: listing.id, booking_time: Time.strptime('06/30/2012 00:00', '%m/%d/%Y %H:%M'))
