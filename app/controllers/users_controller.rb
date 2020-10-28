28bc838627f92e5fda5517ca6f2858423dbb1f82
ChristyTropila <tropila04@gmail.com>
2020-10-28 14:37:08 -0400

    create users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+
+    def index
+        @users=User.all
+        render json: @users
+    end
+
+    def create
+        @user=User.create!(user_params)
+        if @user.valid?
+            session[:user_id]=@user.id
+            render json: @user
+        else
+            render json: {error: "USERNAME IS TAKEN! TRY AGAIN"}, status: 422
+    end
+end
+
+private
+
+def user_params
+    params.permit(:name, :email, :password, :age, :contact)
+end
+
+
 end
