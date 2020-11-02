c24a0d3fe3a057ff224c99aef56e91a22a0f3ef1
ChristyTropila <tropila04@gmail.com>
2020-11-01 20:44:23 -0500

    add review works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,14 @@
 class ReviewsController < ApplicationController
+
+    def index
+        @reviews=Review.all
+        render json: @reviews
+    end
+
+    def create
+        @review=Review.create!(listing_id: params[:listing_id], user_id: params[:user_id], review_text: params[:textarea], rating: params[:rating])
+        
+        @listing=Listing.all.find(params[:listing_id])
+        render json: @listing
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,8 @@ Rails.application.routes.draw do
   get '/users/:id', to: 'users#show'
   post '/users', to: 'users#create'
   post '/users/login', to: 'users#login'
+
+  get '/reviews', to: 'reviews#index'
+  post '/reviews', to: 'reviews#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
